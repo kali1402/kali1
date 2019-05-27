d3944a83829299676d10a65338b9a4d5b06bdb00
kali1402 <amcopeew68@naver.com>
2019-05-27 14:01:44 +0900

    문제풀기

---
+++ 1.js
@@ -0,0 +1,47 @@
+// 1. var 를 let, const 로 대체 했을때의 장점을 설명하십시오.
+
+
+
+// 2. 조원들의 이름, 나이, 생일의 객체를 가진 배열을 생성하십시오.
+
+const chobby = {
+    name : '김우빈',
+    age : 18,
+    birth : '3/12'
+};
+
+const lrving = {
+    name : '문서준',
+    age : 18,
+    birth : '5/23'
+};
+
+const kali = {
+    name : '김현준',
+    age : 18,
+    birth : '8/13'
+};
+
+const sunny = {
+    name : '박수진',
+    age : 18,
+    birth : '10/22'
+};
+const karen = {
+    name : '이다빈',
+    age : 18,
+    birth : '12/10'
+}
+
+const user = [
+    chobby, lrving, kali, sunny, karen
+];
+
+// 3. 반복문을 사용하여 위에 생성한 배열의 조원들 이름을 출력해봅니다.
+
+for(const names of user){
+    console.log('----------------------------');
+    console.log('이름 : '+names.name);
+    console.log('나이 : '+names.age);
+    console.log('생일 : '+names.birth);
+}
